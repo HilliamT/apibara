0540f633c19c0623752e10bb91685fc19985da10
hkt18 <hkt18@ic.ac.uk>
2022-07-12 21:56:13 +0100

    refactor: added details on ChainProvider

--- apibara/src/chain/provider.rs
+++ apibara/src/chain/provider.rs
@@ -13,6 +13,8 @@ use crate::chain::{
 };
 
 /// Provide information about blocks and events/logs on a chain.
+/// Send and Sync are used to allow for concurrency,
+/// refer to https://doc.rust-lang.org/nomicon/send-and-sync.html
 #[async_trait]
 pub trait ChainProvider: Send + Sync + 'static {
     /// Get the most recent (head) block.
@@ -21,6 +23,7 @@ pub trait ChainProvider: Send + Sync + 'static {
     /// Get a specific block by its hash.
     async fn get_block_by_hash(&self, hash: &BlockHash) -> Result<Option<BlockHeader>>;
 
+    /// Subscribe to blocks via stream
     fn subscribe_blocks(&self) -> Result<Pin<Box<dyn Stream<Item = BlockHeader> + Send>>>;
 
     async fn get_events_in_range(
